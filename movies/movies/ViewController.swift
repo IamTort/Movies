7dff1617b2ca2dbc922f874598d3f6b1de49456f
IamTort <61164603+IamTort@users.noreply.github.com>
2022-10-24 18:31:26 +0300

    init

---
+++ movies/movies/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  movies
+//
+//  Created by angelina on 24.10.2022.
+//
+
+import UIKit
+
+///  ViewController
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
